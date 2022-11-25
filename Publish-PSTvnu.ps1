95924dea80cc4d920e7891ac22a890a65fc72fe9
trackd <tracked@gmail.com>
2022-11-25 12:30:11 +0100

    publish stuff

--- Publish-PSTvnu.ps1
+++ Publish-PSTvnu.ps1
@@ -1,4 +1,4 @@
-﻿#cleanup temp json files.
-$ModulePath = "$PSScriptRoot\PSTvnu"
+﻿$ModulePath = "$PSScriptRoot\PSTvnu"
+#cleanup temp json files before publishing module.
 Remove-Item $ModulePath\Private\*.json
 Publish-Module -Path $ModulePath -NuGetApiKey $env:nugetapikey
